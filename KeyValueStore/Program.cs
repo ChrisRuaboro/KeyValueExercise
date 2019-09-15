7034fe096579dc61d639305c600d272f60253632
Christopher Ruaboro <Christopher.Ruaboro@outlook.com>
2019-09-14 18:44:24 -0700

    Add project files.

---
+++ KeyValueStore/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeyValueStore
+{
+    struct KeyValue
+    {
+        public string Key { get; }
+        public object Value { get; }
+
+        public KeyValue(string key, object value)
+        {
+            this.Key = key;
+            this.Value = value;
+
+        }
+    }
+    public class MyDictionary
+    {
+        public int storedValues;
+        KeyValue[] arr = new KeyValue[7];
+        public object this[string key]
+        {
+            get
+            {
+                //iterate through arr and returning value if found
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].Key == key)
+                        return arr[i].Value;
+                }
+                // throws exception if no key matches in arr
+                throw new KeyNotFoundException(key);
+            }
+            set
+            {
+                //searches thru array, overwrites key if found
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    //
+                    if (arr[i].Key == key)
+                    {
+                        arr[i] = new KeyValue(key, value);
+
+                    }
+                }
+                //searched thru array, finds first empty spot, sets new KeyValue, 
+                //adds to counter, and breaks out of loop the new value is set.
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].Key == null)
+                    {
+                        arr[i] = new KeyValue(key, value);
+                        storedValues++;
+                        return;
+                    }
+                }
+            }
+        }
+
+
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+        }
+    }
+}
